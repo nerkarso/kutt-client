4892f113ee1156e5b1439aa818559ab5a1bab608
nerkarso <nerpkarso@gmail.com>
2023-03-06 12:32:56 -0300

    add fetch links query

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,33 @@ import {
   useQuery,
 } from '@tanstack/react-query';
 
+function fetchLinks() {
+  return fetch(
+    'https://kutt.it/api/v2/links?all=false&limit=10&search=&skip=0',
+    {
+      headers: {
+        authorization:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBcGlBdXRoIiwic3ViIjoiZGV0ZWNsb3dAZ21haWwuY29tIiwiZG9tYWluIjoiIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE2NzgxMTI3NjgsImV4cCI6MTY3ODcxNzU2OH0.4yS0P4aDAKZSzxAVOwVrxROP3WtUqJOVVY0aiU2QwZs',
+        cookie:
+          'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBcGlBdXRoIiwic3ViIjoiZGV0ZWNsb3dAZ21haWwuY29tIiwiZG9tYWluIjoiIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE2NzgxMTI3NjgsImV4cCI6MTY3ODcxNzU2OH0.4yS0P4aDAKZSzxAVOwVrxROP3WtUqJOVVY0aiU2QwZs',
+      },
+    }
+  ).then((res) => res.json());
+}
+
 function App() {
   const { isLoading, error, data } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>
-      fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
-        (res) => res.json()
-      ),
+    queryKey: ['links'],
+    queryFn: fetchLinks,
   });
+
+  console.log('Data', data);
 
   if (isLoading) return 'Loading...';
 
   if (error) return 'An error has occurred: ' + error.message;
 
-  return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>{data.subscribers_count}</strong>{' '}
-      <strong>{data.stargazers_count}</strong>{' '}
-      <strong>{data.forks_count}</strong>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
